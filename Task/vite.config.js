ee3ea6c426f98b7733cf3acd6c21c88bdfffbd59
ayush raj pathak <ayushraj.pathak@w3villa.com>
2026-07-06 17:17:28 +0530

    .

--- Task/vite.config.js
+++ Task/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/w3villa-ayushraj-pathak/Task1/",
+  base: "/Task1/",
 })
